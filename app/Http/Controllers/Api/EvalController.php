b714468130964a078ecf9eb02aa5a81afc3570d5
Meracle Tinambacan <meracle.tinambacan@awsys-i.com>
2023-10-03 14:53:43 +0800

    added eval table

---
+++ app/Http/Controllers/Api/EvalController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Evaluation;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class EvalController extends Controller
+{
+    public function indexEval()
+    {
+        $evals = Evaluation::all();
+        if($evals->count()>0){
+
+            return response()->json([
+                'status' => 200,
+                'evals' => $evals
+            ], 200);
+        }else{
+
+            return response()->json([
+                'status' => 404,
+                'message' => 'No records found!'
+            ], 404);
+        }
+
+        
+    }
+
+    public function storeEval(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'projectName' => 'required|string|max:191',
+            'evalPeriod' => 'required|string|max:191',
+            'workLoc' => 'required|string|max:191',
+            'projectMembers' => 'required|string|max:191',
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->errors()
+            ],422);
+        }else{
+
+            $eval = Evaluation::create([
+                'projectName' => $request->projectName,
+                'evalPeriod' => $request->evalPeriod,
+                'workLoc' => $request->workLoc,
+                'projectMembers' => $request->projectMembers,
+            ]);
+
+            if($eval){
+                return response()->json([
+                    'status' => 200,
+                    'message' => "indexEval created Successfully"
+                ], 200);
+            }else{
+
+                return response()->json([
+                    'status' => 500,
+                    'message' => "Something went wrong"
+                ], 500);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EvalController;
 use App\Http\Controllers\Api\EmployeeController;
 
 
@@ -12,3 +13,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('employees', [EmployeeController:: class, 'index']);
 Route::post('employees', [EmployeeController:: class, 'store']);
+Route::get('evals', [EvalController:: class, 'indexEval']);
+Route::post('evals', [EvalController:: class, 'storeEval']);
